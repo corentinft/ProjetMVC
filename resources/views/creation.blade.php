cb7f0214b0bfd465143b3e612be5bdaa07d18177
Bufaloo <killian-r@live.fr>
2020-02-14 11:15:11 +0100

    New creation page

---
+++ resources/views/creation.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.default')
+
+@section('title', 'Creation')
+.full-height {
+    height: 77.15vh;
+}
+
+.flex-center {
+    align-items: center;
+    display: flex;
+    justify-content: center;
+}
+@section('style')
+
+@endsection
+<div class="container full-height flex-center">
+    <div class="dropdown">
+        <button type="button">hello world</button>
+    </div>
+
+</div>
+@section('content')
+
+@endsection
